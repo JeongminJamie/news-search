19fb7bb935f76bf5e481d77aae8bcbe359c6b4be
JeongminJamie <sksrhcalsu@gmail.com>
2024-05-14 21:29:38 +0900

    set up the component layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import DateFilter from "./components/Filter/DateFilter";
+import NewsList from "./components/News/NewsList";
+import SearchBar from "./components/Filter/SearchBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SearchBar />
+      <DateFilter />
+      <NewsList />
+    </>
   );
 }
 
