7a3d809934a7eaae4b62aeb3dc2bb8f659e41e27
JeongminJamie <sksrhcalsu@gmail.com>
2024-05-15 22:19:33 +0900

    done with date filer with react date-picker

--- src/components/News/NewsList.jsx
+++ src/components/News/NewsList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./NewsList.css";
 
 const NewsList = () => {
-  return <div></div>;
+  return <div className="news-list--container"></div>;
 };
 
 export default NewsList;
